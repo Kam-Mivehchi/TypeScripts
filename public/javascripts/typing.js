8b48063584b0982d3949620d9002f93d9c6af448
Ace <omarace95@gmail.com>
2022-03-25 06:27:34 -0700

    typing game fetchs data but doesnt currently render

--- public/javascripts/typing.js
+++ public/javascripts/typing.js
@@ -3,35 +3,28 @@ const typingDiv = document.getElementById("typing");
 const statsDiv = document.getElementById("stats");
 const startGameBtn = document.getElementById("start-game");
 
-// Typing Options
-const typescript = [
-  'TypeScript is a superset of JavaScript which primarily provides optional static typing, classes and interfaces. One of the big benefits is to enable IDEs to provide a richer environment for spotting common errors as you type the code.',
-  'Typically an array contains zero to many objects of a single type. TypeScript has special analysis around arrays which contain multiple types, and where the order in which they are indexed is important. These are called tuples.\nconst failingResponse = ["Not Found", 404];'
-];
-
-const javascript = [
-  'Simple button created in Javascript.\n <button type="button" onclick="document.getElementById("demo").innerHTML = Date()">Click me to display Date and Time.</button>',
-  '<p>In this case JavaScript changes the value of the src (source) attribute of an image.</p>\n <button onclick="document.getElementById("myImage").src="pic_bulbon.gif"`>Turn on the light</button>\n<img id="myImage" src="pic_bulboff.gif" style="width:100px">\n<button onclick="document.getElementById("myImage").src="pic_bulboff.gif"`>Turn off the light</button>'
-];
-
-const html = [
-  '<!DOCTYPE html>\n<html>\n<head>\n<title>Page Title</title>\n</head>\n<body>\n<h1>This is a Heading</h1>\n<p>This is a paragraph.</p>\n</body>\n</html>',
-  '<h2>HTML Links</h2>\n<p>HTML links are defined with the a tag:</p>\n<a href="https://www.w3schools.com">This is a link</a>'
-];
-
-const css = [
-  'The selector points to the HTML element you want to style. The declaration block contains one or more declarations separated by semicolons. Each declaration includes a CSS property name and a value, separated by a colon. Multiple CSS declarations are separated with semicolons, and declaration blocks are surrounded by curly braces.\np {\ncolor: red;\ntext-align: center;\n}',
-  'The universal selector uses the asterisk (i.e *) symbol for denoting the selector as a universal selector. It can be used to select all elements and apply a specific style.\n* {\nproperty: value;\n}'
-]
-
-const english = [
-  `"You don't have to be French to enjoy a decent red wine," Charles Jousselin de Gruse used to tell his foreign guests whenever he entertained them in Paris. "But you do have to be French to recognize one," he would add with a laugh.`,
-  'She was walking lazily, for the fierce April sun was directly overhead. Her umbrella blocked its rays but nothing blocked the heat - the sort of raw, wild heat that crushes you with its energy.',
-]
-
 // Language Choice
 let source = [];
 
+//Finds Prompt based on language selection
+async function getPrompt(language){
+  // Language Choice
+  let source = [];
+  //local scope
+  let languageSearch = "http://localhost:3001/prompts/"+language;
+  fetch(languageSearch,{})
+  .then(function (response){
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data){
+    console.log(data)
+    let source = data.prompt
+    console.log(source)
+    return source
+  })
+}
+  
 // Checks for language choice
 $(document).on("click", ".language", function () {
   let language = $(this).attr("data-id")
@@ -50,19 +43,33 @@ const startGame = (language) => {
 
   // Parses Language
   if (language == "typescript") {
-    source = typescript
+    language_id_num = 1;
   } else if (language == "javascript") {
-    source = javascript
+    language_id_num = 2;
   } else if (language == "html") {
-    source = html
+    language_id_num = 3;
   } else if (language == "css") {
-    source = css
+    language_id_num = 4;
   } else if (language == "english") {
-    source = english
+    language_id_num = 5;
   }
 
+  //Get Prompts
+  getPrompt(language);
+  
+// // Populate Prompts
+// router.get(`/${language}`, async function (req, res) {
+//   const exams = await Prompt.findOne({
+//     where: { language_id: `${language_id_num}`},
+//     order: Sequelize.literal('RAND()'),
+//   }).then(function (examPrompt) {
+//     res.json(examPrompt)
+//   })
+//   console.log(examPrompt.prompt)
+// });
+
   // Randomly Selects from Choice Language
-  const text = source[parseInt(Math.random() * source.length)];
+  const text = source;
 
   // console.log(text) // Would display chosen text in Console
 
